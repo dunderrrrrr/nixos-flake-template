83a0db3d0ede41059f4a7daf14b0d7270153c801
Emil <emil@personalkollen.se>
2024-03-25 07:23:00 +0100

    NixOS flake template

---
+++ README.md
@@ -0,0 +1,5 @@
+## NixOS flake template
+
+```
+git clone https://github.com/dunderrrrrr/nixos-flake-template.git && cd nixos-flake-template && direnv allow
+```
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        (pkgs.python312.withPackages (p: [
+          p.requests
+          p.flask
+        ]))
+        pkgs.nmap
+      ];
+    };
+  };
+}
